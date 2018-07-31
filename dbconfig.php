ac1f226921a670d45af2b1eac56b4d15c15afcda
PGalicia <galicia.patrick@gmail.com>
2018-07-31 12:22:32 -0700

    Uploaded E-Commerce Website Code

---
+++ dbconfig.php
@@ -0,0 +1,7 @@
+<!-- Database Credentials -->
+<?php 
+	define ("DB_USER", 'root');
+	define ("DB_PASSWORD", '1Oathkeeper');
+	define ("DB_DATABASE", 'inf124'); 
+	define ("DB_HOST", 'localhost');
+?>
